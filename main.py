bd2beb12e897a568f59f1731a38c4478709b961c
GraineDeMoutarde <31447965+GraineDeMoutarde@users.noreply.github.com>
2020-07-02 21:08:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+
+
+def parseDaily():
+	text = open('daily.txt', 'r')
+	text.close()
+	return [{"nom": "sport", "debut": "05h00", "fin": "06h00"}]
+
+planning = parseDaily()
+
+def checkinterval_df(newtask):
+	for task in planning:
+		if "debut" in task.keys() and newtask["debut"] < task["debut"] < newtask["fin"]:
+			return False
+		if "fin" in task.keys() and newtask["debut"] < task["fin"] < newtask["fin"]:
+			return False
+		if "debut" in task.keys() and "fin" in task.keys() and newtask["fin"] > task["debut"] and newtask["debut"] < task["fin"]:
+			return False
+	return True
+
+def checkinterval_d(newtask):
+	for task in planning:
+		if "debut" in task.keys() and "fin" in task.keys() and task["debut"] < newtask["debut"] < task["fin"]:
+			return False
+		if "fin" in task.keys() and task["fin"] > newtask["debut"]:
+			return False
+	return True
+
+def checkinterval_f(newtask):
+	for task in planning:
+		if "debut" in task.keys() and "fin" in task.keys() and task["debut"] < newtask["fin"] < task["fin"]:
+			return False
+		if "debut" in task.keys() and task["debut"] < newtask["fin"]:
+			return False
+	return True
+
+
+def ajouter():
+	newtask = {}
+
+	print("Ajout d'un élément")
+	nom = input("nom: ")
+	newtask["nom"] = nom
+
+	choixinterval = 0
+	while choixinterval == 0:
+		print("Autres paramètres (debut, fin)")
+		params = input("").split(", ")
+		for p in params:
+			key, value = p.split(": ")
+			newtask[key] = value
+
+		if "debut" in newtask.keys() and "fin" in newtask.keys():
+			if checkinterval_df(newtask):
+				choixinterval = 1
+
+		if "debut" in newtask.keys() and "fin" not in newtask.keys():
+			if checkinterval_d(newtask):
+				choixinterval = 1
+
+		if "debut" not in newtask.keys() and "fin" in newtask.keys():
+			if checkinterval_f(newtask):
+				choixinterval = 1
+
+
+	planning.append(newtask)
+	print(newtask)
+
+
+
+
+def retirer():
+	pass
+
+daily = parseDaily()
+
+
+ajouter()
